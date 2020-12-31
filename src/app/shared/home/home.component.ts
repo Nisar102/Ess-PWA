144e2ebd8bd9717886aaa978aa7b093abd87ae23
Nisar102 <nisar9981@gmail.com>
2020-12-31 10:11:56 +0500

    ok

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,22 +1,14 @@
-
-import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: 'home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
+  constructor() { }
 
-
-  constructor(private route: Router) { }
-
-  ngOnInit(): void {
-    // this.service._getVoucherByVoucherNo('BP-000012').subscribe((r: any) => {
-    //   console.log('home component' + JSON.stringify(r));
-    // });
-  }
-
+  ngOnInit() { }
 }
